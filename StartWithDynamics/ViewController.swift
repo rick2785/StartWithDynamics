33a8a6391e24bf0bc3eaead2ac337950a7a4b382
Rickey Hrabowskie <rickeyhrabowskie@Rickeys-MacBook-Pro.local>
2016-10-29 06:58:23 -0500

    first commit

--- StartWithDynamics/ViewController.swift
+++ StartWithDynamics/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollisionBehaviorDelegate {
 
+    @IBOutlet weak var groundView: UIView!
+    
+    var animator:UIDynamicAnimator!
+    var ball:UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        ball = UIView(frame: CGRect(x: 100, y: 100, width: 50, height: 50))
+        ball.layer.cornerRadius = 25
+        ball.backgroundColor = UIColor.red
+        
+        self.view.addSubview(ball)
+        
+        animator = UIDynamicAnimator(referenceView: self.view)
+        addGravity()
     }
 
+    func addGravity() {
+        
+        let gravity = UIGravityBehavior(items: [ball])
+        animator.addBehavior(gravity)
+        
+        let collision = UICollisionBehavior(items: [ball])
+        collision.addBoundary(withIdentifier: 1 as NSCopying, from: groundView.frame.origin, to: CGPoint(x: groundView.frame.origin.x + groundView.frame.width, y: groundView.frame.origin.y))
+        
+        animator.addBehavior(collision)
+        
+        collision.collisionDelegate = self
+        
+        let ballBehavior = UIDynamicItemBehavior(items: [ball])
+        ballBehavior.elasticity = 0.75
+        
+        animator.addBehavior(ballBehavior)
+    }
+    
+    func collisionBehavior(_ behavior: UICollisionBehavior, beganContactFor item: UIDynamicItem, withBoundaryIdentifier identifier: NSCopying?, at p: CGPoint) {
+        if NSNumber(integerLiteral: 1).isEqual(identifier) {
+            ball.backgroundColor = UIColor.blue 
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
